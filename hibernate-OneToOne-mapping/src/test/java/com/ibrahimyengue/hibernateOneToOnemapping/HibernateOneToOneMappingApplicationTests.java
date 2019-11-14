fd4ccaa0c9b2f99439f31fdb2b3395716f536f16
Ibrahima bah <25622050+Ibrahimyengue@users.noreply.github.com>
2019-11-14 14:40:35 -0500

    hibernate mapping

---
+++ hibernate-OneToOne-mapping/src/test/java/com/ibrahimyengue/hibernateOneToOnemapping/HibernateOneToOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimyengue.hibernateOneToOnemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOneToOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
